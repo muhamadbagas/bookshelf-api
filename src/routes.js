b52315f8aaf47731ce0565fedd15eb4973f40bf0
Muhamad Bagas Widyatama <bagasmuhamad665@gmail.com>
2022-12-13 20:54:30 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const { addBookHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
